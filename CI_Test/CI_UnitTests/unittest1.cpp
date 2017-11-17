c139fef071c0b59d2cf5362dffa6ff0e7f53c9a9
hitesh <200hitesh@gmail.com>
2017-11-17 11:51:23 +0000

    test added

---
+++ CI_Test/CI_UnitTests/unittest1.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CI_UnitTests
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+
+			Assert::AreEqual(1, 1);
+		}
+
+	};
+}
